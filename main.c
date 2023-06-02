bfc523097d88e6e8983cae5cea3e4737421c2ba8
Aleksandrpl <93161121+Aleksandrpl@users.noreply.github.com>
2023-06-02 19:16:05 +0200

    w metod

--- main.c
+++ main.c
@@ -187,11 +187,9 @@ void podtrzymanie_test(){
     for(;;){
         i++;
     	putchars("Test podtrzymania:");
-		store_os1(0x700F);
-		RAW_SPACE(0x0552)= 0x5555; //zapisanie wartosci na zly adres
-        if(i==10000){
-    		break;
-    	}
+		store_os1 (0x700F );
+        RAW_SPACE (0x0574)= 0x5555 ;
+        
     }
 }
 
